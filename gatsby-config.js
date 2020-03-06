b6099846f18687ed70873d3e25ba4fb207a031da
Jerome BEHUET <jbehuet@gmail.com>
2020-03-06 20:08:43 +0100

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,12 @@ module.exports = {
           }
         ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-38322348-4'
+      }
     }
   ]
 };
